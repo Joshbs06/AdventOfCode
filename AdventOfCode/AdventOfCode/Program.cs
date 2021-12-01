fc07690484b8c3dd63430643cabf9378172f47f0
Josh Birks <josh.birks@redrow.co.uk>
2021-12-01 13:00:33 +0000

    Completed part 1 2021 and attempted part 2

---
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AdventOfCode2021;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of Code \n");
+            First();
+        }
+
+        private static void First()
+        {
+            Console.WriteLine("!st December 2021 \n");
+
+            var listOfInts = WriteFileToListOfInts();
+            var first2021 = new First();
+            var amountOfIncreases = first2021.CalculateAmountOfIncreases(listOfInts);
+            var amountSumOf3Increases = first2021.CalculateAmountOfSumIncreases(listOfInts);
+
+            Console.WriteLine($"There are {amountOfIncreases} increases");
+            Console.WriteLine($"There are {amountSumOf3Increases} sum of 3 increases");
+        }
+
+        private static List<int> WriteFileToListOfInts()
+        {
+            return File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "Resources/2021/FirstDecember.txt")).Select(int.Parse).ToList();
+        }
+    }
+}
--- AdventOfCode/AdventOfCode2021/First.cs
+++ AdventOfCode/AdventOfCode2021/First.cs
@@ -1,6 +1,42 @@
-﻿namespace AdventOfCode2021
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2021
 {
     public class First
     {
+        public int CalculateAmountOfIncreases(List<int> values)
+        {
+            int amountOfIncreases = 0;
+
+            for (int i = 1; i < values.Count; i++)
+            {
+                if (values[i] > values[i - 1])
+                {
+                    amountOfIncreases++;
+                }
+            }
+
+            return amountOfIncreases;
+        }
+
+        public int CalculateAmountOfSumIncreases(List<int> values)
+        {
+            int amountOfSumIncreases = 0;
+
+            for (int i = 1; i < (values.Count - 2); i++)
+            {
+                amountOfSumIncreases += (SumOf3Values(values[i - 1], values[i], values[i + 1]) < SumOf3Values(values[i], values[i + 1], values[i + 2]))
+                    ? 1
+                    : 0;
+            }
+
+            return amountOfSumIncreases;
+        }
+
+        private int SumOf3Values(int a, int b, int c)
+        {
+            return a + b + c;
+        }
     }
 }
